9ced614c0150da3832c101e268e591cff2d7f87a
Minassyants <Killka1997@gmail.com>
2024-08-13 10:20:25 +0500

    123

--- src/main/java/mb/pso/issuesystem/config/SecurityConfiguration.java
+++ src/main/java/mb/pso/issuesystem/config/SecurityConfiguration.java
@@ -1,48 +1,48 @@
-package mb.pso.issuesystem.config;
+// package mb.pso.issuesystem.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
-import mb.pso.issuesystem.service.impl.UserServiceImpl;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.authentication.AuthenticationProvider;
+// import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.web.SecurityFilterChain;
+// import static org.springframework.security.config.Customizer.withDefaults;
+// import mb.pso.issuesystem.service.impl.UserServiceImpl;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfiguration {
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfiguration {
 
-    final UserServiceImpl userServiceImpl;
+//     final UserServiceImpl userServiceImpl;
 
-    public SecurityConfiguration(UserServiceImpl userServiceImpl) {
-        this.userServiceImpl = userServiceImpl;
-    }
+//     public SecurityConfiguration(UserServiceImpl userServiceImpl) {
+//         this.userServiceImpl = userServiceImpl;
+//     }
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated()).httpBasic(withDefaults())
-                .formLogin(withDefaults());
-        return http.build();
+//     @Bean
+//     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//         http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated()).httpBasic(withDefaults())
+//                 .formLogin(withDefaults());
+//         return http.build();
 
-    }
+//     }
 
-    @Bean
-    PasswordEncoder passwordEncoder()
-    {
-        return new BCryptPasswordEncoder();
-    }
+//     @Bean
+//     PasswordEncoder passwordEncoder()
+//     {
+//         return new BCryptPasswordEncoder();
+//     }
 
-    @Bean
-    AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userServiceImpl);
-        provider.setPasswordEncoder(passwordEncoder());
-        return provider;
-    }
+//     @Bean
+//     AuthenticationProvider authenticationProvider(){
+//         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//         provider.setUserDetailsService(userServiceImpl);
+//         provider.setPasswordEncoder(passwordEncoder());
+//         return provider;
+//     }
 
-}
+// }
