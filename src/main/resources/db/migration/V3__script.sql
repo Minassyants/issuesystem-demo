cb40f8ea49d0f54e9c5cd4ee9a18fffa6494fa18
Minassyants <Killka1997@gmail.com>
2024-08-28 08:15:38 +0500

    123

---
+++ src/main/resources/db/migration/V3__script.sql
@@ -0,0 +1,4 @@
+ALTER TABLE if exists issue ALTER COLUMN if exists department_feedback TYPE TEXT;
+ALTER TABLE if exists issue ALTER COLUMN if exists issue_description TYPE TEXT;
+ALTER TABLE if exists issue ALTER COLUMN if exists issued_demands TYPE TEXT;
+ALTER TABLE if exists issue ALTER COLUMN if exists issue_result TYPE TEXT;
---
+++ src/main/resources/db/migration/V4__script.sql
@@ -0,0 +1,2 @@
+ALTER TABLE if exists good ALTER COLUMN if exists description TYPE TEXT;
+ALTER TABLE if exists vehicle ALTER COLUMN if exists description TYPE TEXT;
