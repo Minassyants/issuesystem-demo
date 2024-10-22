446970e6db610d8b02ba4b7fd94cf82b4c99ff71
Minassyants <Killka1997@gmail.com>
2024-10-22 16:49:52 +0500

    123

--- src/main/java/mb/pso/issuesystem/controller/im/ChatController.java
+++ src/main/java/mb/pso/issuesystem/controller/im/ChatController.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import mb.pso.issuesystem.dto.webSocket.SocketMsg;
 import mb.pso.issuesystem.entity.Employee;
 import mb.pso.issuesystem.entity.im.Message;
 import mb.pso.issuesystem.service.impl.ImServiceImpl;
@@ -23,19 +24,22 @@ public class ChatController {
     @MessageMapping("/chat/sendmessage")
     public void sendMessage(Message message) {
         Message msg = imServiceImpl.sendMessage(message);
-        simpMessagingTemplate.convertAndSend("/topic/chat/" + msg.getChat().getId().toString(), msg);
+        simpMessagingTemplate.convertAndSend("/topic/chat/" + msg.getChat().getId().toString(),
+                new SocketMsg(SocketMsg.MsgType.NEWMESSAGE, msg));
     }
 
     @MessageMapping("/chat/{chatId}/addmembertochat")
     public void addMemberToChat(@DestinationVariable Integer chatId, Employee employee) {
         Employee addedEmployee = imServiceImpl.addMemberToChat(chatId, employee);
-        simpMessagingTemplate.convertAndSend("/topic/chat/" + chatId + "/addmember", addedEmployee);
+        simpMessagingTemplate.convertAndSend("/topic/chat/" + chatId + "/addmember",
+                new SocketMsg(SocketMsg.MsgType.ADDEDMEMBER, addedEmployee));
     }
 
     @MessageMapping("/chat/{chatId}/deletememberfromchat")
     public void deleteMemberFromChat(@DestinationVariable Integer chatId, Employee employee) {
         Employee deletedEmployee = imServiceImpl.deleteMemberFromChat(chatId, employee);
-        simpMessagingTemplate.convertAndSend("/topic/chat/" + chatId + "/deletemember", deletedEmployee);
+        simpMessagingTemplate.convertAndSend("/topic/chat/" + chatId + "/deletemember",
+                new SocketMsg(SocketMsg.MsgType.DELETEDMEMBER, deletedEmployee));
     }
 
 }
