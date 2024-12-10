07e6d20939df2ce34c3f3cdd7da20c195c718181
Minassyants <105652196+Minassyants@users.noreply.github.com>
2024-12-10 16:38:48 +0500

    Update WebConfig.java

--- src/main/java/mb/pso/issuesystem/config/WebConfig.java
+++ src/main/java/mb/pso/issuesystem/config/WebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class WebConfig {
     private final String allowerdOriginPatterns;
 
@@ -21,8 +21,10 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOriginPatterns(allowerdOriginPatterns.split(","))
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowedHeaders("*")
+                        .exposedHeaders("*")
                         .allowCredentials(true);
             }
         };
